381658474cf26cddef4e02af647040fb0044925f
Fernando Esparza <fernando.esparza02@utrgv.edu>
2022-11-22 16:53:47 -0600

    Add games data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int64_value, bool_value, string_value) VALUES (9458700342, 1, 'Hello,  Mr. Loki') 
+
+--UPDATE sandbox SET double_value = -2.1543054 WHERE id = 120;
+
+--DELETE FROM sandbox WHERE id = 97;
+
+SELECT * FROM sandbox;
